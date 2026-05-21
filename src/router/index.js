8be70ca55d24a4262fd4f68825f1234f63ecb5cb
cecis <cecistudy23@gamil.com>
2026-05-21 15:30:30 -0300

    up

--- src/router/index.js
+++ src/router/index.js
@@ -44,7 +44,10 @@ const routes = [{
   component: Cabecalho
 }]
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+
+  history: createWebHistory(import.meta.env.BASE_URL),
+
+  routes,
+
 })
 export default router

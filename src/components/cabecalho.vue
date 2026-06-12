9f214c38646c0fff40094e8a1a701b1cea8d55d0
Gabriel Rosa <gabriel@rosa.dev.br>
2026-06-12 16:02:41 -0300

    ✨ Dark mode adicionado

--- src/components/cabecalho.vue
+++ src/components/cabecalho.vue
@@ -1,30 +1,39 @@
 <script setup>
   import { Icon } from '@iconify/vue'
+  import { useStorage } from '@vueuse/core'
+  import { watch } from 'vue'
+
+  const isDark = useStorage('is-dark', true)
 
   function darkMode() {
     const html = document.documentElement
-    html.classList.toggle('dark')
+    if (isDark.value) {
+      html.classList.add('dark')
+    } else {
+      html.classList.remove('dark')
+    }
   }
 
+  watch(isDark, darkMode, { immediate: true })
 </script>
 
 <template>
-    <header class="flex flex-wrap items-center justify-between p-2.25 sm:w-full  bg-rose-100 mb-1
-    dark:bg-rose-950/90">
-      <div>
-        <h1 class="text-xl font-bold text-rose-950 dark:text-rose-400">Meu Painel</h1>
-        <p class="text-xs text-rose-800 dark:text-rose-200">Atividades Realizadas</p>
-      </div>
-        <div class="text-right">
-          <h1 class="text-sm text-rose-950 dark:text-rose-400">Russaneta</h1> 
-          <p class="text-xs text-rose-800 dark:text-rose-200">Nivel de Acesso</p>
-          <button class="bg-rose-200 rounded-full p-2 mt-2" @click="darkMode">
-          <Icon
-            icon="line-md:moon-alt-loop"
-            class="text-2xl text-rose-900"
-          />
-        </button>
-        </div>
-    </header>
-  
+  <header
+    class="mb-1 flex flex-wrap items-center justify-between bg-rose-100 p-2.25 sm:w-full dark:bg-rose-950/90">
+    <div>
+      <h1 class="text-xl font-bold text-rose-950 dark:text-rose-400">Meu Painel</h1>
+      <p class="text-xs text-rose-800 dark:text-rose-200">Atividades Realizadas</p>
+    </div>
+    <div class="text-right">
+      <h1 class="text-sm text-rose-950 dark:text-rose-400">Russaneta</h1>
+      <p class="text-xs text-rose-800 dark:text-rose-200">Nivel de Acesso</p>
+      <button class="mt-2 rounded-full bg-rose-200 p-2" @click="isDark = !isDark">
+        <Icon
+          :icon="
+            isDark ? 'line-md:moon-alt-loop' : 'line-md:moon-filled-to-sunny-filled-loop-transition'
+          "
+          class="text-2xl text-rose-900" />
+      </button>
+    </div>
+  </header>
 </template>
